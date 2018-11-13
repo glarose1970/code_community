26a5f41f7c113f6c3b03de4213bb567e2ca0ba1a
Gene <g.larose@frontier.com>
2018-11-12 23:48:26 -0500

    testing cmd line

--- views.py
+++ views.py
@@ -5,5 +5,8 @@ from django.http import HttpResponse
 
 
 def index(request):
-    my_dict = {'insert_me': 'Now I am from first_app/index.html'}
+    my_dict = {'insert_me': 'Now I am from first_app/index.html',
+               't_one': 'added from views.py',
+               't_two': 'second insert',
+               't_three': 'third insert into table', }
     return render(request, 'first_app/index.html', context=my_dict)
