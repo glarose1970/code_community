c87fd4ab0c89b77a85879b3174d668488dddc29f
Gene <g.larose@frontier.com>
2018-11-12 23:50:58 -0500

    more testing

--- views.py
+++ views.py
@@ -1,8 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-# Create your views here.
-
 
 def index(request):
     my_dict = {'insert_me': 'Now I am from first_app/index.html',
